c538360b438f5a51fb7f186231d62638106e663a
jenkins96 <adrianjnkns@gmail.com>
2021-02-13 03:19:47 +0000

    added more values in arrays

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
-//console.log("Any string");
 
-let pronoun = ['the','our'];
-let adj = ['great', 'big' ];
-let noun = ['jogger','racoon'];
+// Variables Declaration
+let pronoun = ["the", "our", "you", "they", "it"];
+let adj = ["great", "big", "pretty", "awesome", "conservative"];
+let noun = ["jogger", "racoon", "mountain", "music", "ships"];
+let domain = [".be", ".es", ".org", ".int", ".tel", ".tv", ".travel", ".biz"]
 
+// Using forEach();
 pronoun.forEach(item1 => {
     adj.forEach(item2 => {
         noun.forEach(item3 => {
-            let output = item1.concat(item2.concat(item3, ".com"));
-            console.log(output);
-        })
+            domain.forEach(item4 => {
+                let output = item1.concat(item2.concat(item3.concat(item4)));
+                console.log(output);
+            });
+            
+        });
     });
 });
 
